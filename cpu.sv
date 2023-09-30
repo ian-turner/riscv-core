c42e5a4ecb0a9bf1e403f54266b03b3bd3f6b96e
Ian Turner <i.r.turner55@gmail.com>
2023-09-30 13:05:12 -0400

    basic cpu skeleton

---
+++ cpu.sv
@@ -0,0 +1,17 @@
+module cpu(input logic clk /* clock */, input logic rst_n /* reset signal */);
+
+	logic [31:0] inst_ram [4095:0]; // declaring ram
+	initial $readmemh("program.rom", inst_ram); // reading program into memory
+
+	logic [11:0] PC_FETCH = 12'd0;
+	logic [31:0] instruction_EX;
+
+	always_ff @(posedge clk) begin
+		if (~rst_n) begin // starting at 0
+			PC_FETCH <= 12'd0;
+			instruction_EX <= 32'd0;
+		end else begin // fetching the next instruction
+			PC_FETCH <= PC_FETCH + 1'b1;
+			instruction_EX <= inst_ram[PC_FETCH];
+	end
+endmodule
