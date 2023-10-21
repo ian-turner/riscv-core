f9e6bcbfdd3faa87b1a70b487c845c806d801e04
Ian Turner <i.r.turner55@gmail.com>
2023-10-21 13:07:21 -0400

    implemented slli and finished bin2dec.asm

--- bin2dec.asm
+++ bin2dec.asm
@@ -2,7 +2,7 @@
 
 addi x1, zero, 25		# getting our value for 0.1
 slli x1, x1, 8			# this is done by placing each 8 bits and 
-				# shifting since the real value is too large
+				# shifting since the real value is too large for U-type imm
 addi x2, zero, 153		
 or x1, x1, x2			# <-- second 8 bits
 slli x1, x1, 8
@@ -11,10 +11,13 @@ slli x1, x1, 8
 addi x2, zero, 154
 or x1, x1, x2			# <-- fourth 8 bits
 
-addi x5, zero, 10		# setting x5 to 10 to use later
+addi x5, zero, 10		# setting x5 to 10 to use later for mul
 				
-addi x2, zero, 1829		# our arbitrary input value
-				# (hard-coded for now)
+#addi x2, zero, 1829		# arbitrary input value
+				# (hard-coded)
+				
+
+csrrw x2, 0, zero		# reading input value from switches
 
 # digit 1
 mul x3, x2, x1			# multiply value by 0.1 to get fractional bits
@@ -22,7 +25,7 @@ mul x3, x2, x1			# multiply value by 0.1 to get fractional bits
 mulhu x2, x2, x1		# getting the hi bits and setting to x2
 mulhu x6, x3, x5		# multiply fractional bits by 10 to get number
 
-# digit 2
+# digit 2			# repeat process for each 8 digits
 slli x6, x6, 4
 mul x3, x2, x1
 mulhu x2, x2, x1
@@ -70,3 +73,5 @@ mul x3, x2, x1
 mulhu x2, x2, x1
 mulhu x7, x3, x5
 or x6, x6, x7
+
+csrrw zero, 0, x6		# writing back out to hex
--- controlunit.sv
+++ controlunit.sv
@@ -62,6 +62,7 @@ module controlunit(
 			if (funct3==3'b111) aluop=4'b0011; // andi
 			if (funct3==3'b110) aluop=4'b0001; // ori
 			if (funct3==3'b100) aluop=4'b0010; // xori
+			if (funct3==3'b001) aluop=4'b1000; // slli
 		end
 
 		// U-type instructions
